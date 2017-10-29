b61763392b3b39f9c0e2be3b31d79dfd3b2f404d
37bhushan <37bhushan@gmail.com>
2017-10-29 08:35:38 +0530

    Initial Commit

---
+++ GravityBody.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+
+    GravityAttractor planet;
+    new Rigidbody rigidbody;
+
+    void Awake()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+        rigidbody = GetComponent<Rigidbody>();
+        //  Debug.Log("got RigidBody");
+        // Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
+        rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        // Allow this body to be influenced by planet's gravity
+        planet.Attract(rigidbody);
+    }
+}
